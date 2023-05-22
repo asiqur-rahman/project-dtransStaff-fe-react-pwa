8b1d46fb8733763813bb6bc7e2e7ebf1c248694d
Asiqur Rahman Khan <asiqur.rahman@datasoft-bd.com>
2023-05-22 17:02:38 +0600

    aa

--- dtrans-staff/src/components/Category.jsx
+++ dtrans-staff/src/components/Category.jsx
@@ -307,91 +307,7 @@ function All() {
                 {/* <!-- Item box Start --> */}
 
                 <div className="item-list recent-jobs-list">
-                  <ul>
-                    <li>
-                      <div className="item-content">
-                        <div className="item-inner">
-                          <div className="item-title-row">
-                            <h6 className="item-title"><a href="product-detail.html">Cappucino Latte Creamy Milk Italiano</a></h6>
-                            <div className="item-subtitle">Coffe, Milk</div>
-                          </div>
-                          <div className="item-footer">
-                            <div className="d-flex align-items-center">
-                              <h6 className="me-3">$ 5.0</h6>
-                              <del className="off-text"><h6>$ 8.9</h6></del>
-                            </div>
-                            <div className="d-flex align-items-center">
-                              <i className="fa-solid fa-star"></i>
-                              <h6>4.5</h6>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="item-media media media-90"><img src="/images/food/pic1.png" alt="logo" />
-                          <a href="javascript:void(0);" className="item-bookmark icon-2">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M16.785 2.04751C15.9489 2.04694 15.1209 2.21163 14.3486 2.53212C13.5764 2.85261 12.8751 3.32258 12.285 3.91501L12 4.18501L11.73 3.91501C11.1492 3.2681 10.4424 2.74652 9.65306 2.3822C8.86367 2.01787 8.00824 1.81847 7.13912 1.79618C6.27 1.7739 5.40547 1.9292 4.59845 2.25259C3.79143 2.57599 3.05889 3.06066 2.44566 3.67695C1.83243 4.29325 1.35142 5.02819 1.03206 5.83682C0.712696 6.64544 0.561704 7.51073 0.588323 8.37973C0.614942 9.24873 0.818613 10.1032 1.18687 10.8907C1.55513 11.6783 2.08022 12.3824 2.73002 12.96L12 22.2675L21.3075 12.96C22.2015 12.0677 22.8109 10.9304 23.0589 9.6919C23.3068 8.45338 23.1822 7.16915 22.7006 6.00144C22.2191 4.83373 21.4023 3.83492 20.3534 3.13118C19.3045 2.42744 18.0706 2.05034 16.8075 2.04751H16.785Z" fill="white" />
-                            </svg>
-                          </a>
-                        </div>
-                      </div>
-                    </li>
-                    <li>
-                      <div className="item-content">
-                        <div className="item-inner">
-                          <div className="item-title-row">
-                            <h6 className="item-title"><a href="product-detail.html">Hot Mochacinno Coffee Grande</a></h6>
-                            <div className="item-subtitle">Coffe</div>
-                          </div>
-                          <div className="item-footer">
-                            <div className="d-flex align-items-center">
-                              <h6 className="me-3">$ 7.5</h6>
-                              <del className="off-text"><h6>$ 8.9</h6></del>
-                            </div>
-                            <div className="d-flex align-items-center">
-                              <i className="fa-solid fa-star"></i>
-                              <h6>4.0</h6>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="item-media media media-90"><img src="/images/food/pic2.png" alt="logo" />
-                          <a href="javascript:void(0);" className="item-bookmark icon-2">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M16.785 2.04751C15.9489 2.04694 15.1209 2.21163 14.3486 2.53212C13.5764 2.85261 12.8751 3.32258 12.285 3.91501L12 4.18501L11.73 3.91501C11.1492 3.2681 10.4424 2.74652 9.65306 2.3822C8.86367 2.01787 8.00824 1.81847 7.13912 1.79618C6.27 1.7739 5.40547 1.9292 4.59845 2.25259C3.79143 2.57599 3.05889 3.06066 2.44566 3.67695C1.83243 4.29325 1.35142 5.02819 1.03206 5.83682C0.712696 6.64544 0.561704 7.51073 0.588323 8.37973C0.614942 9.24873 0.818613 10.1032 1.18687 10.8907C1.55513 11.6783 2.08022 12.3824 2.73002 12.96L12 22.2675L21.3075 12.96C22.2015 12.0677 22.8109 10.9304 23.0589 9.6919C23.3068 8.45338 23.1822 7.16915 22.7006 6.00144C22.2191 4.83373 21.4023 3.83492 20.3534 3.13118C19.3045 2.42744 18.0706 2.05034 16.8075 2.04751H16.785Z" fill="white" />
-                            </svg>
-                          </a>
-                        </div>
-                      </div>
-                    </li>
-                    <li>
-                      <div className="item-content">
-                        <div className="item-inner">
-                          <div className="item-title-row">
-                            <h6 className="item-title"><a href="product-detail.html">Coffee Mocha / White Mocha</a></h6>
-                            <div className="item-subtitle">Coffe, Milk</div>
-                          </div>
-                          <div className="item-footer">
-                            <div className="d-flex align-items-center">
-                              <h6 className="me-3">$ 5.0</h6>
-                              <del className="off-text"><h6>$ 8.9</h6></del>
-                            </div>
-                            <div className="d-flex align-items-center">
-                              <i className="fa-solid fa-star"></i>
-                              <h6>4.3</h6>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="item-media media media-90"><img src="/images/food/pic3.png" alt="logo" />
-                          <a href="javascript:void(0);" className="item-bookmark icon-2">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M16.785 2.04751C15.9489 2.04694 15.1209 2.21163 14.3486 2.53212C13.5764 2.85261 12.8751 3.32258 12.285 3.91501L12 4.18501L11.73 3.91501C11.1492 3.2681 10.4424 2.74652 9.65306 2.3822C8.86367 2.01787 8.00824 1.81847 7.13912 1.79618C6.27 1.7739 5.40547 1.9292 4.59845 2.25259C3.79143 2.57599 3.05889 3.06066 2.44566 3.67695C1.83243 4.29325 1.35142 5.02819 1.03206 5.83682C0.712696 6.64544 0.561704 7.51073 0.588323 8.37973C0.614942 9.24873 0.818613 10.1032 1.18687 10.8907C1.55513 11.6783 2.08022 12.3824 2.73002 12.96L12 22.2675L21.3075 12.96C22.2015 12.0677 22.8109 10.9304 23.0589 9.6919C23.3068 8.45338 23.1822 7.16915 22.7006 6.00144C22.2191 4.83373 21.4023 3.83492 20.3534 3.13118C19.3045 2.42744 18.0706 2.05034 16.8075 2.04751H16.785Z" fill="white" />
-                            </svg>
-                          </a>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                  <div className="saprater"></div>
-                  <h4 className="title my-4">Cold Brew</h4>
+                  <h4 className="title my-4">Details</h4>
 
                   <ul>
                     <li>
@@ -412,13 +328,7 @@ function All() {
                             </div>
                           </div>
                         </div>
-                        <div className="item-media media media-90"><img src="/images/food/pic4.png" alt="logo" />
-                          <a href="javascript:void(0);" className="item-bookmark icon-2">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M16.785 2.04751C15.9489 2.04694 15.1209 2.21163 14.3486 2.53212C13.5764 2.85261 12.8751 3.32258 12.285 3.91501L12 4.18501L11.73 3.91501C11.1492 3.2681 10.4424 2.74652 9.65306 2.3822C8.86367 2.01787 8.00824 1.81847 7.13912 1.79618C6.27 1.7739 5.40547 1.9292 4.59845 2.25259C3.79143 2.57599 3.05889 3.06066 2.44566 3.67695C1.83243 4.29325 1.35142 5.02819 1.03206 5.83682C0.712696 6.64544 0.561704 7.51073 0.588323 8.37973C0.614942 9.24873 0.818613 10.1032 1.18687 10.8907C1.55513 11.6783 2.08022 12.3824 2.73002 12.96L12 22.2675L21.3075 12.96C22.2015 12.0677 22.8109 10.9304 23.0589 9.6919C23.3068 8.45338 23.1822 7.16915 22.7006 6.00144C22.2191 4.83373 21.4023 3.83492 20.3534 3.13118C19.3045 2.42744 18.0706 2.05034 16.8075 2.04751H16.785Z" fill="white" />
-                            </svg>
-                          </a>
-                        </div>
+                        <div className="item-media media media-60"><img src="/images/food/pic4.png" alt="logo" /></div>
                       </div>
                     </li>
                     <li>
@@ -435,17 +345,11 @@ function All() {
                             </div>
                             <div className="d-flex align-items-center">
                               <i className="fa-solid fa-star"></i>
-                              <h6>4.5</h6>
+                              <h6><button></button></h6>
                             </div>
                           </div>
                         </div>
-                        <div className="item-media media media-90"><img src="/images/food/pic5.png" alt="logo" />
-                          <a href="javascript:void(0);" className="item-bookmark icon-2">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M16.785 2.04751C15.9489 2.04694 15.1209 2.21163 14.3486 2.53212C13.5764 2.85261 12.8751 3.32258 12.285 3.91501L12 4.18501L11.73 3.91501C11.1492 3.2681 10.4424 2.74652 9.65306 2.3822C8.86367 2.01787 8.00824 1.81847 7.13912 1.79618C6.27 1.7739 5.40547 1.9292 4.59845 2.25259C3.79143 2.57599 3.05889 3.06066 2.44566 3.67695C1.83243 4.29325 1.35142 5.02819 1.03206 5.83682C0.712696 6.64544 0.561704 7.51073 0.588323 8.37973C0.614942 9.24873 0.818613 10.1032 1.18687 10.8907C1.55513 11.6783 2.08022 12.3824 2.73002 12.96L12 22.2675L21.3075 12.96C22.2015 12.0677 22.8109 10.9304 23.0589 9.6919C23.3068 8.45338 23.1822 7.16915 22.7006 6.00144C22.2191 4.83373 21.4023 3.83492 20.3534 3.13118C19.3045 2.42744 18.0706 2.05034 16.8075 2.04751H16.785Z" fill="white" />
-                            </svg>
-                          </a>
-                        </div>
+                        <div className="item-media media media-60"><img src="/images/food/pic4.png" alt="logo" /></div>
                       </div>
                     </li>
                   </ul>
