0e8ed788e278b2b25d74a7294d048a95fad9b6c1
Asiqur Rahman Khan <asiqur.rahman@datasoft-bd.com>
2023-05-30 10:55:24 +0600

    aa

--- src/pages/OTPPage.jsx
+++ src/pages/OTPPage.jsx
@@ -1,22 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from '../utils/axios.utils';
-import * as common from '../utils/common.utils';
-import { useNavigate } from 'react-router-dom';
 import ResetPasswordPage from './ResetPasswordPage';
 import { toast } from 'react-toastify';
 
 function HomePage(props) {
-  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [showResetPassword, setShowResetPassword] = useState(false);
+
   const [otp1, setOtp1] = useState('');
   const [otp2, setOtp2] = useState('');
   const [otp3, setOtp3] = useState('');
   const [otp4, setOtp4] = useState('');
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     setUsername(props.username)
-  },[props])
+  }, [props])
 
   const otpConfirm = async (e) => {
     window.SpinnerShow()
@@ -25,10 +23,10 @@ function HomePage(props) {
       toast.error("Please provide OTP");
       return;
     }
-    else{
+    else {
       let body = {
         username: username,
-        otp: otp1+otp2+otp3+otp4,
+        otp: otp1 + otp2 + otp3 + otp4,
       };
       try {
         const result = await axios.post('accounts/validate-reset-otp', body);
@@ -45,26 +43,107 @@ function HomePage(props) {
     window.SpinnerHide()
   };
 
+  const inputRefs = [
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null)
+  ];
+
+  const handleOtpChange = (index, value) => {
+    switch (index) {
+      case 0:
+        setOtp1(value);
+        if (value.length === 1) {
+          inputRefs[1].current.focus();
+        }
+        break;
+      case 1:
+        setOtp2(value);
+        if (value.length === 1) {
+          inputRefs[2].current.focus();
+        }
+        break;
+      case 2:
+        setOtp3(value);
+        if (value.length === 1) {
+          inputRefs[3].current.focus();
+        }
+        break;
+      case 3:
+        setOtp4(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleKeyDown = (index, event) => {
+    if (event.key === 'Backspace' && event.target.value === '') {
+      if (index > 0) {
+        inputRefs[index - 1].current.focus();
+      }
+    }
+  };
+
   return (
-      <>
-      {showResetPassword && <ResetPasswordPage username={username} otp={otp1+otp2+otp3+otp4}/>}
-      {!showResetPassword && 
-      <div className="account-box">
-        <div className="container">
+    <>
+      {showResetPassword && <ResetPasswordPage username={username} otp={otp1 + otp2 + otp3 + otp4} />}
+      {!showResetPassword &&
+        <div className="account-box">
+          <div className="container">
             <div className="account-area">
-                <div method="get" id="otp" className="digit-group" data-group-name="digits" data-autosubmit="false" autoComplete="off">
-                    <input className="form-control" value={otp1} maxLength={1} onChange={(e)=>setOtp1(e.target.value)} type="text" id="digit-1" name="digit-1" placeholder="-" data-next="digit-2" />
-                    <input className="form-control" value={otp2} maxLength={1} onChange={(e)=>setOtp2(e.target.value)} type="text" id="digit-2" name="digit-2" placeholder="-" data-next="digit-3" data-previous="digit-1" />
-                    <input className="form-control" value={otp3} maxLength={1} onChange={(e)=>setOtp3(e.target.value)} type="text" id="digit-3" name="digit-3" placeholder="-" data-next="digit-4" data-previous="digit-2" />
-                    <input className="form-control" value={otp4} maxLength={1} onChange={(e)=>setOtp4(e.target.value)} type="text" id="digit-4" name="digit-4" placeholder="-" data-next="digit-5" data-previous="digit-3" />
-                </div>                
-                <div className="input-group">
-                    <button type="button" className="btn mt-3 btn-primary w-100 btn-rounded" onClick={otpConfirm}>VERIFY & PROCEED</button>
-                </div>
+              <div method="get" id="otp" className="digit-group" data-group-name="digits" data-autosubmit="false" autoComplete="off">
+                <input
+                  className="form-control"
+                  value={otp1}
+                  maxLength={1}
+                  onChange={(e) => handleOtpChange(0, e.target.value)}
+                  onKeyDown={(e) => handleKeyDown(0, e)}
+                  type="text"
+                  ref={inputRefs[0]}
+                  placeholder="-"
+                />
+                <input
+                  className="form-control"
+                  value={otp2}
+                  maxLength={1}
+                  onChange={(e) => handleOtpChange(1, e.target.value)}
+                  onKeyDown={(e) => handleKeyDown(1, e)}
+                  type="text"
+                  ref={inputRefs[1]}
+                  placeholder="-"
+                />
+                <input
+                  className="form-control"
+                  value={otp3}
+                  maxLength={1}
+                  onChange={(e) => handleOtpChange(2, e.target.value)}
+                  onKeyDown={(e) => handleKeyDown(2, e)}
+                  type="text"
+                  ref={inputRefs[2]}
+                  placeholder="-"
+                />
+                <input
+                  className="form-control"
+                  value={otp4}
+                  maxLength={1}
+                  onChange={(e) => handleOtpChange(3, e.target.value)}
+                  onKeyDown={(e) => handleKeyDown(3, e)}
+                  type="text"
+                  ref={inputRefs[3]}
+                  placeholder="-"
+                />
+              </div>
+              <div className="input-group">
+                <button type="button" className="btn mt-3 btn-primary w-100 btn-rounded" onClick={otpConfirm}>
+                  VERIFY & PROCEED
+                </button>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-    }</>
+      }</>
   );
 }
 
