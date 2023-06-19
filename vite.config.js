db8dcce0ff37cd56ac9f6af2bc32122db8db0406
Asiqur Rahman Khan <asiqur.rahman@datasoft-bd.com>
2023-06-19 15:50:56 +0600

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { https: true },
   plugins: [
+    mkcert(),
     react(),
     VitePWA({
       registerType: 'auto',
