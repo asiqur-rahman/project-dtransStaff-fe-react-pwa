715a75672b01b4a18841446a5571d68db38e560e
Md. Asiqur Rahman Khan <asiqur.rahman.khan.0@gmail.com>
2023-06-27 11:57:30 +0600

    aa

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const Config = {
       SESSION_NAME : "DTrans-Staff",
       googleMapsApiKey: "AIzaSyD9i-g54Pyo62YC6Zc_DoUtJ1uOUhJKYVM",
       pushServerPublicKey : "BB5EDm4B6fa6yVlvn8hSjvNSMYYt3CvgDnDfNKnBTbueasPkcWdV8Tak1roJG_gDeEZOo_6jvx1Qka1QRnsII_M",
-      pushServerBaseUrl:"http://localhost:9000/",
+      pushServerBaseUrl:"http://test.braintechsolution.com/",
     }
 }
 
