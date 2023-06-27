a9d874e30949d8bf4d0940c421d75a526f4a1b38
Md. Asiqur Rahman Khan <asiqur.rahman.khan.0@gmail.com>
2023-06-27 12:17:18 +0600

    aa

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
 const Config = {
     MODE: "development",
-    applicationInfo:{
-      appName: "Sus-Plan",
-      devOrgName: "Bangladesh Institute of Management (BIM)",
-      devOrgLink: "http://www.bim.gov.bd/"
-    },
     applicationSettings:{
       developmentBaseURL : "https://devb2b.bizpoint-intl.com:3086/",
       productionBaseURL : "https://devb2b.bizpoint-intl.com:3086/",
